7d1e6e1b9aa164a3878dea22151ca6b7131d66b1
ailyntang <ailyntang@gmail.com>
2019-04-18 20:24:19 +1000

    Add encapsulating `<div id='projects>`
    Required this id tag for CSS.
    
    Originally required for freeCodeCamp test scripts.

--- script.js
+++ script.js
@@ -146,58 +146,67 @@ var Portfolio = function (_React$Component3) {
         null,
         React.createElement(
           'div',
-          { id: 'project-tiles' },
-          React.createElement(Project, {
-            projectLink: 'https://ailyntang.github.io/pomodoro/',
-            imageLink: 'https://images.unsplash.com/37/tEREUy1vSfuSu8LzTop3_IMG_2538.jpg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
-            imageText: 'Alarm clock',
-            overlayTextLine1: 'Pomodoro Timer',
-            overlayTextLine2: '(React)' }),
-          React.createElement(Project, {
-            projectLink: 'https://codepen.io/ailyntang/full/aaGeoz',
-            imageLink: 'https://images.pexels.com/photos/56876/queen-cup-honeycomb-honey-bee-new-queen-rearing-compartment-56876.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-            imageText: 'Bees in a beehive',
-            overlayTextLine1: 'Create a hexagon mural',
-            overlayTextLine2: '(Javascript)' }),
-          React.createElement(Project, {
-            projectLink: 'https://codepen.io/ailyntang/full/XoqWmw',
-            imageLink: 'https://images.unsplash.com/photo-1506555191898-a76bacf004ca?ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80',
-            imageText: 'Calculator',
-            overlayTextLine1: 'Calculator',
-            overlayTextLine2: '(React)' }),
-          React.createElement(Project, {
-            projectLink: 'https://codepen.io/ailyntang/full/qLbopg',
-            imageLink: 'https://images.unsplash.com/photo-1467049293311-8c2750cda7a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1952&q=80',
-            imageText: 'Camera previewer',
-            overlayTextLine1: 'Markdown Previewer',
-            overlayTextLine2: '(React)' }),
-          React.createElement(Project, {
-            projectLink: 'https://codepen.io/ailyntang/full/oJeLdr',
-            imageLink: 'https://images.unsplash.com/photo-1519892300165-cb5542fb47c7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
-            imageText: 'Drums',
-            overlayTextLine1: 'Drum Machine',
-            overlayTextLine2: '(React)' }),
-          React.createElement(Project, {
-            projectLink: 'https://codepen.io/ailyntang/full/jQxLjJ/',
-            imageLink: 'https://images.pexels.com/photos/688668/pexels-photo-688668.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-            imageText: 'Framed photo of a quote',
-            overlayTextLine1: 'Quote of the day',
-            overlayTextLine2: '(React)' }),
-          React.createElement(Project, {
-            projectLink: 'https://codepen.io/ailyntang/full/PBXQOY/',
-            imageLink: 'https://images.pexels.com/photos/270238/pexels-photo-270238.png?cs=srgb&dl=application-blur-business-270238.jpg&fm=jpg',
-            imageText: 'Magnifying glass over a German document',
-            overlayTextLine1: 'Technical Documentation' }),
-          React.createElement(Project, {
-            projectLink: 'https://codepen.io/ailyntang/full/JZrVbj/',
-            imageLink: 'https://images.pexels.com/photos/263532/pexels-photo-263532.jpeg?cs=srgb&dl=abandoned-antique-architecture-263532.jpg&fm=jpg',
-            imageText: 'Letterbox with a feedback sign',
-            overlayTextLine1: 'Survey' }),
-          React.createElement(Project, {
-            projectLink: 'https://codepen.io/ailyntang/full/WyKEEB/',
-            imageLink: 'https://images.pexels.com/photos/374006/pexels-photo-374006.jpeg?cs=srgb&dl=business-coffee-computer-374006.jpg&fm=jpg',
-            imageText: 'Someone working on a laptop at a desk',
-            overlayTextLine1: 'Product Landing Page' })
+          { id: 'projects' },
+          React.createElement(
+            'h1',
+            null,
+            'Projects'
+          ),
+          React.createElement(
+            'div',
+            { id: 'project-tiles' },
+            React.createElement(Project, {
+              projectLink: 'https://ailyntang.github.io/pomodoro/',
+              imageLink: 'https://images.unsplash.com/37/tEREUy1vSfuSu8LzTop3_IMG_2538.jpg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+              imageText: 'Alarm clock',
+              overlayTextLine1: 'Pomodoro Timer',
+              overlayTextLine2: '(React)' }),
+            React.createElement(Project, {
+              projectLink: 'https://codepen.io/ailyntang/full/aaGeoz',
+              imageLink: 'https://images.pexels.com/photos/56876/queen-cup-honeycomb-honey-bee-new-queen-rearing-compartment-56876.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+              imageText: 'Bees in a beehive',
+              overlayTextLine1: 'Create a hexagon mural',
+              overlayTextLine2: '(Javascript)' }),
+            React.createElement(Project, {
+              projectLink: 'https://codepen.io/ailyntang/full/XoqWmw',
+              imageLink: 'https://images.unsplash.com/photo-1506555191898-a76bacf004ca?ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80',
+              imageText: 'Calculator',
+              overlayTextLine1: 'Calculator',
+              overlayTextLine2: '(React)' }),
+            React.createElement(Project, {
+              projectLink: 'https://codepen.io/ailyntang/full/qLbopg',
+              imageLink: 'https://images.unsplash.com/photo-1467049293311-8c2750cda7a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1952&q=80',
+              imageText: 'Camera previewer',
+              overlayTextLine1: 'Markdown Previewer',
+              overlayTextLine2: '(React)' }),
+            React.createElement(Project, {
+              projectLink: 'https://codepen.io/ailyntang/full/oJeLdr',
+              imageLink: 'https://images.unsplash.com/photo-1519892300165-cb5542fb47c7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+              imageText: 'Drums',
+              overlayTextLine1: 'Drum Machine',
+              overlayTextLine2: '(React)' }),
+            React.createElement(Project, {
+              projectLink: 'https://codepen.io/ailyntang/full/jQxLjJ/',
+              imageLink: 'https://images.pexels.com/photos/688668/pexels-photo-688668.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+              imageText: 'Framed photo of a quote',
+              overlayTextLine1: 'Quote of the day',
+              overlayTextLine2: '(React)' }),
+            React.createElement(Project, {
+              projectLink: 'https://codepen.io/ailyntang/full/PBXQOY/',
+              imageLink: 'https://images.pexels.com/photos/270238/pexels-photo-270238.png?cs=srgb&dl=application-blur-business-270238.jpg&fm=jpg',
+              imageText: 'Magnifying glass over a German document',
+              overlayTextLine1: 'Technical Documentation' }),
+            React.createElement(Project, {
+              projectLink: 'https://codepen.io/ailyntang/full/JZrVbj/',
+              imageLink: 'https://images.pexels.com/photos/263532/pexels-photo-263532.jpeg?cs=srgb&dl=abandoned-antique-architecture-263532.jpg&fm=jpg',
+              imageText: 'Letterbox with a feedback sign',
+              overlayTextLine1: 'Survey' }),
+            React.createElement(Project, {
+              projectLink: 'https://codepen.io/ailyntang/full/WyKEEB/',
+              imageLink: 'https://images.pexels.com/photos/374006/pexels-photo-374006.jpeg?cs=srgb&dl=business-coffee-computer-374006.jpg&fm=jpg',
+              imageText: 'Someone working on a laptop at a desk',
+              overlayTextLine1: 'Product Landing Page' })
+          )
         ),
         React.createElement(Contact, null)
       );
--- src/script.jsx
+++ src/script.jsx
@@ -56,66 +56,71 @@ class Portfolio extends React.Component {
   render() {
     return(
       <div>
-        <div id='project-tiles'>
-          <Project
-          projectLink='https://ailyntang.github.io/pomodoro/'
-          imageLink='https://images.unsplash.com/37/tEREUy1vSfuSu8LzTop3_IMG_2538.jpg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80'
-          imageText='Alarm clock'
-          overlayTextLine1='Pomodoro Timer'
-          overlayTextLine2='(React)'/>
+        
+        <div id='projects'>
+          <h1>Projects</h1>
+          
+          <div id='project-tiles'>
+            <Project
+            projectLink='https://ailyntang.github.io/pomodoro/'
+            imageLink='https://images.unsplash.com/37/tEREUy1vSfuSu8LzTop3_IMG_2538.jpg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80'
+            imageText='Alarm clock'
+            overlayTextLine1='Pomodoro Timer'
+            overlayTextLine2='(React)'/>
 
-          <Project
-          projectLink='https://codepen.io/ailyntang/full/aaGeoz'
-          imageLink='https://images.pexels.com/photos/56876/queen-cup-honeycomb-honey-bee-new-queen-rearing-compartment-56876.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
-          imageText='Bees in a beehive'
-          overlayTextLine1='Create a hexagon mural'
-          overlayTextLine2='(Javascript)'/>
+            <Project
+            projectLink='https://codepen.io/ailyntang/full/aaGeoz'
+            imageLink='https://images.pexels.com/photos/56876/queen-cup-honeycomb-honey-bee-new-queen-rearing-compartment-56876.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+            imageText='Bees in a beehive'
+            overlayTextLine1='Create a hexagon mural'
+            overlayTextLine2='(Javascript)'/>
 
-          <Project
-          projectLink='https://codepen.io/ailyntang/full/XoqWmw'
-          imageLink='https://images.unsplash.com/photo-1506555191898-a76bacf004ca?ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80'
-          imageText='Calculator'
-          overlayTextLine1='Calculator'
-          overlayTextLine2='(React)'/>
+            <Project
+            projectLink='https://codepen.io/ailyntang/full/XoqWmw'
+            imageLink='https://images.unsplash.com/photo-1506555191898-a76bacf004ca?ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80'
+            imageText='Calculator'
+            overlayTextLine1='Calculator'
+            overlayTextLine2='(React)'/>
 
-          <Project
-          projectLink='https://codepen.io/ailyntang/full/qLbopg'
-          imageLink='https://images.unsplash.com/photo-1467049293311-8c2750cda7a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1952&q=80'
-          imageText='Camera previewer'
-          overlayTextLine1='Markdown Previewer'
-          overlayTextLine2='(React)'/>
+            <Project
+            projectLink='https://codepen.io/ailyntang/full/qLbopg'
+            imageLink='https://images.unsplash.com/photo-1467049293311-8c2750cda7a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1952&q=80'
+            imageText='Camera previewer'
+            overlayTextLine1='Markdown Previewer'
+            overlayTextLine2='(React)'/>
 
-          <Project
-          projectLink='https://codepen.io/ailyntang/full/oJeLdr'
-          imageLink='https://images.unsplash.com/photo-1519892300165-cb5542fb47c7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80'
-          imageText='Drums'
-          overlayTextLine1='Drum Machine'
-          overlayTextLine2='(React)'/>
+            <Project
+            projectLink='https://codepen.io/ailyntang/full/oJeLdr'
+            imageLink='https://images.unsplash.com/photo-1519892300165-cb5542fb47c7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80'
+            imageText='Drums'
+            overlayTextLine1='Drum Machine'
+            overlayTextLine2='(React)'/>
 
-          <Project
-          projectLink='https://codepen.io/ailyntang/full/jQxLjJ/'
-          imageLink='https://images.pexels.com/photos/688668/pexels-photo-688668.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
-          imageText='Framed photo of a quote'
-          overlayTextLine1='Quote of the day'
-          overlayTextLine2='(React)'/>
+            <Project
+            projectLink='https://codepen.io/ailyntang/full/jQxLjJ/'
+            imageLink='https://images.pexels.com/photos/688668/pexels-photo-688668.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+            imageText='Framed photo of a quote'
+            overlayTextLine1='Quote of the day'
+            overlayTextLine2='(React)'/>
 
-          <Project
-          projectLink='https://codepen.io/ailyntang/full/PBXQOY/'
-          imageLink='https://images.pexels.com/photos/270238/pexels-photo-270238.png?cs=srgb&dl=application-blur-business-270238.jpg&fm=jpg'
-          imageText='Magnifying glass over a German document'
-          overlayTextLine1='Technical Documentation'/>
+            <Project
+            projectLink='https://codepen.io/ailyntang/full/PBXQOY/'
+            imageLink='https://images.pexels.com/photos/270238/pexels-photo-270238.png?cs=srgb&dl=application-blur-business-270238.jpg&fm=jpg'
+            imageText='Magnifying glass over a German document'
+            overlayTextLine1='Technical Documentation'/>
 
-          <Project
-          projectLink='https://codepen.io/ailyntang/full/JZrVbj/'
-          imageLink='https://images.pexels.com/photos/263532/pexels-photo-263532.jpeg?cs=srgb&dl=abandoned-antique-architecture-263532.jpg&fm=jpg'
-          imageText='Letterbox with a feedback sign'
-          overlayTextLine1='Survey'/>
+            <Project
+            projectLink='https://codepen.io/ailyntang/full/JZrVbj/'
+            imageLink='https://images.pexels.com/photos/263532/pexels-photo-263532.jpeg?cs=srgb&dl=abandoned-antique-architecture-263532.jpg&fm=jpg'
+            imageText='Letterbox with a feedback sign'
+            overlayTextLine1='Survey'/>
 
-          <Project
-          projectLink='https://codepen.io/ailyntang/full/WyKEEB/'
-          imageLink='https://images.pexels.com/photos/374006/pexels-photo-374006.jpeg?cs=srgb&dl=business-coffee-computer-374006.jpg&fm=jpg'
-          imageText='Someone working on a laptop at a desk'
-          overlayTextLine1='Product Landing Page'/>
+            <Project
+            projectLink='https://codepen.io/ailyntang/full/WyKEEB/'
+            imageLink='https://images.pexels.com/photos/374006/pexels-photo-374006.jpeg?cs=srgb&dl=business-coffee-computer-374006.jpg&fm=jpg'
+            imageText='Someone working on a laptop at a desk'
+            overlayTextLine1='Product Landing Page'/>
+          </div>
         </div>
 
         <Contact />
