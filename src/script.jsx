380f84e9dd9f06be4ba540457923157ab4f1e159
ailyntang <ailyntang@gmail.com>
2019-04-18 19:40:04 +1000

    Add pomodoro project to Portfolio class

--- src/script.jsx
+++ src/script.jsx
@@ -24,6 +24,18 @@ class Portfolio extends React.Component {
     super(props);
   }
 
+  render() {
+    return(
+      <div id='project-tiles'>
+        <Project
+        projectLink='https://ailyntang.github.io/pomodoro/'
+        imageLink='https://images.unsplash.com/37/tEREUy1vSfuSu8LzTop3_IMG_2538.jpg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80'
+        imageText='Alarm clock'
+        overlayTextLine1='Pomodoro Timer'
+        overlayTextLine2='(React)'/>
+      </div>
+    );
+  }
 };
 
 ReactDOM.render(<Portfolio />, document.getElementById('app'));
