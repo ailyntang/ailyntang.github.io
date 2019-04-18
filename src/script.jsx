b62f0a7e6697a2009b92ea254576718933cb448b
ailyntang <ailyntang@gmail.com>
2019-04-18 19:38:59 +1000

    Create Project component
    New Project component in React

--- src/script.jsx
+++ src/script.jsx
@@ -1,3 +1,24 @@
+class Project extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div className='project-tile'>
+        <a href={this.props.projectLink} target='_blank'>
+          <img className='project-img' src={this.props.imageLink} alt={this.props.imageText} />
+          <div className='overlay'>
+            <div className='overlay-text'>
+              {this.props.overlayTextLine1} <br/> {this.props.overlayTextLine2}
+            </div>
+          </div>
+        </a>
+      </div>
+    );
+  }
+};
+
 class Portfolio extends React.Component {
   constructor(props) {
     super(props);
